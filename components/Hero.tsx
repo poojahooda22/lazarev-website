11036d7db3ba4a774773c8babe910911dbe425ee
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-06-25 15:38:15 +0530

    hero component

---
+++ components/Hero.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Hero() {
+  return (
+    <div className='w-full h-[100vh]'>
+        <div className='w-full h-full flex flex-col items-center justify-center text-'>
+            <div>
+                <h1>AI & ML Product</h1>
+            </div>
+            <div>
+                <h1>AI & ML Product</h1>
+            </div>
+
+        </div>
+    </div>
+  )
+}
+
+export default Hero
