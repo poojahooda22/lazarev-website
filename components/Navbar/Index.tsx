f08895b16095a7484d8bd479f55f87eff30376d9
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-06-25 17:54:22 +0530

    created header component

---
+++ components/Navbar/Index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className='sm:max-w-screen-xl mx-auto  py-[4vw] px-[5vw] sm:px-0 sm:py-0'>
+        <div>
+            <h1>Header</h1>
+        </div>
+    </div>
+  )
+}
+
+export default Header
