cb0bfceaf0aea401f0cd113bd4c605db4d86ea5e
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-06-25 15:37:37 +0530

    cursor and hero comp

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 'use client'
 
+import Cursor from '@/components/cursor/Index';
+import Hero from '@/components/Hero';
 import { useRef,  useEffect } from 'react';
 
 
@@ -16,7 +18,8 @@ export default function Home() {
 
   return (
     <main className="">
-      Hello
+      <Cursor />
+      <Hero />
     </main>
   );
 }
