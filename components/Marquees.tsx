0dbeab76ae29e6616f432991bb40966ef136e468
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-06-26 17:27:20 +0530

    created marquee component

---
+++ components/Marquees.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Marquee from './Marquee'
+
+function Marquees() {
+
+    const data = [
+        'https://lazarev.kiev.ua/la24/rd.svg',
+        'https://lazarev.kiev.ua/la24/awww.svg'
+    ]
+  return (
+    <div className=''>
+      <div>
+        <Marquee />
+      </div>
+    </div>
+  )
+}
+
+export default Marquees
