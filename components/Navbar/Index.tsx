9d5a0f0df6d4ac26cc7b298269d13153d709653f
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-06-28 21:53:51 +0530

    updated navbar

--- components/Navbar/Index.tsx
+++ components/Navbar/Index.tsx
@@ -112,8 +112,7 @@ function Header() {
             <div 
                 className={`hidden sm:inline-block absolute 
                     navBottom ${style.navBottom} 
-                    w-[84%] h-[0%] text-[.7vw] bg-black z-[1]
-`}
+                    w-[84%] h-[0%] text-[.7vw] bg-black z-[1]`}
             >  
             </div>
             <div className='hidden sm:inline-block'>
