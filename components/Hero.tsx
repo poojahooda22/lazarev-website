3c25f8f236868c471e71f85e4542b9498b07208e
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-06-28 22:00:39 +0530

    updated hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -49,7 +49,8 @@ function Hero() {
                         sm:px-4 sm:py-2 rounded-full'>brand design</span>
                         <span className='text-md whitespace-nowrap  sm:text-sm 
                         border-[1px] border-grey px-[2vw] py-[1.5vw] 
-                        sm:px-4 sm:py-2 rounded-full'>user experience</span>
+                        sm:px-4 sm:py-2 rounded-full'>
+                            user experience</span>
                     </div> 
                     <div className='mt-[0vw] flex gap-2 items-center'>
                         <span className='text-md sm:text-sm'>and</span>
