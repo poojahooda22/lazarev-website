117931b6d671be14a02a5f0a48a29b2d266eba23
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-06-29 19:42:15 +0530

    updated marquees

--- components/Marquees.tsx
+++ components/Marquees.tsx
@@ -14,9 +14,9 @@ function Marquees() {
       'https://lazarev.kiev.ua/la24/adweek.svg'
     ]
   return (
-    <div className=''>
-      <div>
-        <Marquee />
+    <div className='w-full max-w-screen-xl mx-auto'>
+      <div className='flex items-center justify-center gap-4'>
+        {data.map((item, index) => <Marquee key={index} item={item} />)}
       </div>
     </div>
   )
