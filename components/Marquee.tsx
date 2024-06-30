3d7e5f54ce1d1c423b1498336aa908818e8bfd1a
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-06-30 22:09:33 +0530

    added motion

--- components/Marquee.tsx
+++ components/Marquee.tsx
@@ -3,14 +3,14 @@ import React from 'react'
 
 function Marquee(item: any) {
   return (
-    <div> 
+    <motion.div> 
       <Image 
         src={item.item} 
         alt='Marquee' 
         width={8} height={32} 
         className='w-[3.5vw] h-[2.5vw]'
       />
-    </div>
+    </motion.div>
   )
 }
 
