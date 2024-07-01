b485b96c8e87e734fa434de473727aee29ec3c77
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-07-01 13:37:51 +0530

    updated marquees

--- components/Marquees.tsx
+++ components/Marquees.tsx
@@ -4,7 +4,8 @@ import Marquee from './Marquee'
 function Marquees() {
 
   const data = [
-    'https://lazarev.kiev.ua/la24/rd.svg',
+    [
+      'https://lazarev.kiev.ua/la24/rd.svg',
     'https://lazarev.kiev.ua/la24/awww.svg',
     'https://lazarev.kiev.ua/la24/webby.svg',
     'https://lazarev.kiev.ua/la24/fwa.svg',
@@ -12,11 +13,16 @@ function Marquees() {
     'https://lazarev.kiev.ua/la24/wf.svg',
     'https://lazarev.kiev.ua/la24/pmi.svg',
     'https://lazarev.kiev.ua/la24/adweek.svg',
+    ],
+    [
+      
+    ]
+    
   ]
 
   return (
-    <div className='w-full max-w-screen-xl mx-auto -mt-[2vw] '>
-      <div className='flex items-center justify-center gap-[2vw]'>
+    <div className='w-full max-w-screen-xl mx-auto -mt-[2vw] flex items-center justify-center overflow-hidden'>
+      <div className='w-1/2 flex items-center justify-center gap-[2vw] '>
         {data.map((item, index) => 
           <Marquee 
             key={index} 
