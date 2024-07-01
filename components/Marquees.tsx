594df5284bec99853e90a418c1e54bd4527af0a3
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-07-01 15:25:05 +0530

    updated marquees

--- components/Marquees.tsx
+++ components/Marquees.tsx
@@ -4,8 +4,7 @@ import Marquee from './Marquee'
 function Marquees() {
 
   const data = [
-    [
-      'https://lazarev.kiev.ua/la24/rd.svg',
+    'https://lazarev.kiev.ua/la24/rd.svg',
     'https://lazarev.kiev.ua/la24/awww.svg',
     'https://lazarev.kiev.ua/la24/webby.svg',
     'https://lazarev.kiev.ua/la24/fwa.svg',
@@ -13,22 +12,19 @@ function Marquees() {
     'https://lazarev.kiev.ua/la24/wf.svg',
     'https://lazarev.kiev.ua/la24/pmi.svg',
     'https://lazarev.kiev.ua/la24/adweek.svg',
-    ],
-    [
-      
-    ]
+    'https://lazarev.kiev.ua/la24/pmi.svg',
+    'https://lazarev.kiev.ua/la24/adweek.svg',
     
   ]
 
   return (
-    <div className='w-full max-w-screen-xl mx-auto -mt-[2vw] flex items-center justify-center overflow-hidden'>
-      <div className='w-1/2 flex items-center justify-center gap-[2vw] '>
-        {data.map((item, index) => 
-          <Marquee 
-            key={index} 
-            item={item} 
-          />
-        )}
+    <div className='w-full max-w-screen-xl mx-auto -mt-[2vw] flex items-center justify-center pb-[4vw]'>
+      <div className='w-1/2 flex items-center justify-center gap-[2vw]  overflow-hidden'>
+        {data.map((item, index) => {
+          return (
+            <Marquee key={index} item={item} />
+          )
+        })}
       </div>
     </div>
   )
