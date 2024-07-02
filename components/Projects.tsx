af12f13725aaea7d2d96a2694a13f866588302c4
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-07-02 21:21:55 +0530

    updated projects

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Projects = () => {
   return (
     <div className='w-full'>
-      <div className='max-w-screen-xl mx-auto border-t-[1px] border-grey'>
+      <div className='max-w-screen-xl mx-auto border-t-[1px] border-grey pt-[1vw]'>
         <h1>Projects</h1>
       </div>
     </div>
