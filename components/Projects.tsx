3cabca457cd9fce7b6a137208b969e9068109d5d
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-07-02 21:45:41 +0530

    make div flex

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -3,12 +3,12 @@ import React from 'react'
 const Projects = () => {
   return (
     <div className='w-full h-[30vw] pb-[4vw]'>
-      <div className='w-full h-full max-w-screen-xl mx-auto border-t-[1px] border-grey pt-[1vw]'>
+      <div className='w-full h-full max-w-screen-xl mx-auto border-t-[1px] border-grey pt-[1vw] flex'>
         <div className='w-1/2 h-full flex flex-col items-start justify-between'>
           <div className='w-1/2'>
             <p className='w-1/2'>Exclusive insights into UI/UX and product design</p>
           </div>
-          <div className='w-2/3'>
+          <div className='w-2/4'>
             <p className='text-sm leading-[1.3vw]'>
               Being a design studio at the forefront of digital transformation
               and product design, we take it upon ourselves to drop some knowledge,
@@ -16,7 +16,9 @@ const Projects = () => {
             </p>
           </div>
         </div>
-        <div className='w-1/2'></div>
+        <div className='w-1/2'>
+          <h2>pppp</h2>
+        </div>
       </div>
     </div>
   )
