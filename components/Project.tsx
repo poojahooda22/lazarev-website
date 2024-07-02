e8ba49cdf1e681eabbd6366b0a6ee8d2357663ce
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-07-02 22:29:22 +0530

    updated project

--- components/Project.tsx
+++ components/Project.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Project = () => {
   return (
-    <div className='flex items-start justify-between border-t-[.1px] border-grey'>
+    <div className='flex items-start justify-between border-t-[.1px] -mt-[1.5vw] border-grey pt-[1.5vw]'>
         <div className='w-2/3'>
             <h1 className='text-md leading-[2vw]'>The Gestalt Principles of Design: 
                 How Design Forms visual perception
