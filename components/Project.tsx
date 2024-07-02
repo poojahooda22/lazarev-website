41a81eeaf3daa312753d0d19561234b663a768ca
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-07-02 22:19:55 +0530

    updated projects

--- components/Project.tsx
+++ components/Project.tsx
@@ -3,13 +3,13 @@ import React from 'react'
 const Project = () => {
   return (
     <div className='flex items-start justify-between'>
-        <div className=''>
+        <div className='w-2/3'>
             <h1 className='text-md leading-[2vw]'>The Gestalt Principles of Design: 
                 How Design Forms visual perception
             </h1>
         </div>
         <div>
-            <p>6 min read </p>
+            <p className=''>6 min read </p>
             <div className='w-[2vw]'>
                 <svg fill="none" viewBox="0 0 15 15">
                     <path fill="currentColor" fill-rule="evenodd" d="M10.125 3.153H1.961V1H13.8v11.839h-2.152V4.675L2.722 13.6 1.2 12.078l8.925-8.925Z" clip-rule="evenodd"></path>
