5003261cddbb967ec4acfe53b1a3d62af7dc36f6
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-07-02 21:24:21 +0530

    updated projects

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -5,7 +5,9 @@ const Projects = () => {
     <div className='w-full'>
       <div className='max-w-screen-xl mx-auto border-t-[1px] border-grey pt-[1vw]'>
         <div className='w-1/2'>
-          
+          <div>
+            <p>Exclusive insights into UI/UX and product design</p>
+          </div>
         </div>
         <div className='w-1/2'></div>
       </div>
