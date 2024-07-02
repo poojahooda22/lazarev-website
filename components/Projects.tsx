4a6b8feb41eeaadbe347d7170b984fa30483b90a
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-07-02 21:23:26 +0530

    updated projects

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -4,7 +4,10 @@ const Projects = () => {
   return (
     <div className='w-full'>
       <div className='max-w-screen-xl mx-auto border-t-[1px] border-grey pt-[1vw]'>
-        <h1>Projects</h1>
+        <div className='w-1/2'>
+          
+        </div>
+        <div className='w-1/2'></div>
       </div>
     </div>
   )
