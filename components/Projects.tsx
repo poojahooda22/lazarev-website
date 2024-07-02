3f9d61e021d40616f8ce6ce50d9d8401726e5947
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-07-02 21:37:58 +0530

    updated projects

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -2,17 +2,17 @@ import React from 'react'
 
 const Projects = () => {
   return (
-    <div className='w-full'>
-      <div className='max-w-screen-xl mx-auto border-t-[1px] border-grey pt-[1vw]'>
-        <div className='w-1/2'>
+    <div className='w-full h-[30vw] pb-[4vw]'>
+      <div className='w-full h-full max-w-screen-xl mx-auto border-t-[1px] border-grey pt-[1vw]'>
+        <div className='w-1/2 h-full flex flex-col items-start justify-between'>
           <div>
             <p>Exclusive insights into UI/UX and product design</p>
           </div>
-          <div>
+          <div className='w-2/3'>
             <p>
               Being a design studio at the forefront of digital transformation
               and product design, we take it upon ourselves to drop some knowledge,
-              
+              let out insider tips, and put our work on full display
             </p>
           </div>
         </div>
