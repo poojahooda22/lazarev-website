57cd8ded2e581eadb817303597e22f2007559eea
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-07-02 22:20:57 +0530

    updated

--- components/Project.tsx
+++ components/Project.tsx
@@ -9,8 +9,8 @@ const Project = () => {
             </h1>
         </div>
         <div>
-            <p className=''>6 min read </p>
-            <div className='w-[2vw]'>
+            <p className='text-grey text-xs'>6 min read </p>
+            <div className='w-[1vw]'>
                 <svg fill="none" viewBox="0 0 15 15">
                     <path fill="currentColor" fill-rule="evenodd" d="M10.125 3.153H1.961V1H13.8v11.839h-2.152V4.675L2.722 13.6 1.2 12.078l8.925-8.925Z" clip-rule="evenodd"></path>
                 </svg>
