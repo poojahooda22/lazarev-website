384dcada49aa1ac1ce9963674430c2170a776755
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-07-02 22:17:34 +0530

    added

--- components/Project.tsx
+++ components/Project.tsx
@@ -11,7 +11,8 @@ const Project = () => {
             <p>6 min read </p>
             <div className='w-[2vw]'>
                 <svg fill="none" viewBox="0 0 15 15">
-                    <path fill="currentColor" fill-rule="evenodd" d="M10.125 3.153H1.961V1H13.8v11.839h-2.152V4.675L2.722 13.6 1.2 12.078l8.925-8.925Z" clip-rule="evenodd"></path></svg>
+                    <path fill="currentColor" fill-rule="evenodd" d="M10.125 3.153H1.961V1H13.8v11.839h-2.152V4.675L2.722 13.6 1.2 12.078l8.925-8.925Z" clip-rule="evenodd"></path>
+                    </svg>
             </div>
         </div>
     </div>
