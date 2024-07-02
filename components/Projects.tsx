92b176bfcdecd1742f9b6789c5171bc41471d56d
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-07-02 22:27:18 +0530

    updated padding

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -4,9 +4,9 @@ import Project from './Project'
 const Projects = () => {
   return (
     <div className='w-full h-[30vw] pb-[4vw]'>
-      <div className='w-full h-full max-w-screen-xl mx-auto border-t-[1px] border-grey pt-[1.5vw] flex'>
+      <div className='w-full h-full max-w-screen-xl mx-auto border-t-[.1px] border-grey pt-[1.5vw] flex'>
         <div className='w-1/2 h-full flex flex-col items-start justify-between'>
-          <div className='w-1/2'>
+          <div className='w-1/2 pt-[1vw]'>
             <p className='w-1/2'>Exclusive insights into UI/UX and product design</p>
           </div>
           <div className='w-2/4'>
