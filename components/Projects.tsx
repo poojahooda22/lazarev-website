258c6f722a1b5eeb9fcdf8affd283c129dcd8378
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-07-03 17:33:53 +0530

    updated projects

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -7,27 +7,32 @@ const Projects = () => {
     {
       title: 'The Gestalt Principles of Design: How Design Forms visual perception',
       time: '6 min read',
-      image: '../public/images/cover1.webp'
+      image: '/images/cover1.webp'
     },
     {
       title: 'Effective Design Principles For Crypto Web Design',
       time: '3 min read',
-      image: '../public/images/Cover Img.webp'
+      image: '/images/Cover Img.webp'
     },
     {
-      title: '',
+      title: 'How to design an interface users will trust in 2024?',
       time: '5 min read',
-      image: '../public/images/Cover Img.webp'
+      image: '/images/_cover.webp'
+    },
+    {
+      title: 'how AI influences design and the reciprocal impact of UX on AI-Driven Products',
+      time: '5 min read',
+      image: '/images/Cover3.webp'
     },
 
   ]
 
   return (
-    <div className='w-full h-[30vw] pb-[4vw]'>
-      <div className='w-full h-full max-w-screen-xl mx-auto border-t-[.1px] border-grey pt-[1.5vw] flex'>
-        <div className='w-1/2 h-full flex flex-col items-start justify-between'>
+    <div className='w-full pb-[4vw]'>
+      <div className='w-full  max-w-screen-xl mx-auto border-t-[.1px] border-grey pt-[1.5vw] flex'>
+        <div className='w-1/2 h-[34vw] flex flex-col items-start justify-between'>
           <div className='w-1/2 pt-[1vw]'>
-            <p className='w-1/2'>Exclusive insights into UI/UX and product design</p>
+            <p className='w-1/2 text-[.8vw] leading-[1vw]'>Exclusive insights into<br/> UI/UX and product design</p>
           </div>
           <div className='w-2/4'>
             <p className='text-sm leading-[1.3vw]'>
@@ -38,7 +43,11 @@ const Projects = () => {
           </div>
         </div>
         <div className='w-1/2'>
-          <Project />
+          {data.map((item, index) => {
+            return (
+              <Project key={index} item={item} />
+            )
+          })}  
         </div>
       </div>
     </div>
