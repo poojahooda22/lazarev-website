c151f771a9444ad4816b07dda1da773d65b383ba
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-07-04 21:10:46 +0530

    added

--- components/Marquees.tsx
+++ components/Marquees.tsx
@@ -18,7 +18,7 @@ function Marquees() {
           'https://lazarev.kiev.ua/la24/wf.svg',
           'https://lazarev.kiev.ua/la24/pmi.svg',
           'https://lazarev.kiev.ua/la24/adweek.svg',
-        ],
+      ],
         [    
       ]
     ]
