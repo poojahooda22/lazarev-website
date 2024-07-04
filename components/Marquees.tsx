431f6ae46a2be5f0a1f66109d84e666b0b797604
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-07-04 21:16:37 +0530

    added

--- components/Marquees.tsx
+++ components/Marquees.tsx
@@ -24,7 +24,7 @@ function Marquees() {
     
   return (
     <div 
-    className='w-full max-w-screen-xl mx-auto -mt-[16vw] sm:-mt-[4vw] sm:pb-[4vw] px-[5vw] sm:px-0
+      className='w-full max-w-screen-xl mx-auto -mt-[16vw] sm:-mt-[4vw] sm:pb-[4vw] px-[5vw] sm:px-0
       flex items-center justify-center'>
       <div className='w-full sm:w-2/3 relative'>
         <div className='w-[4vw] h-1/2 sm:w-[3vw] sm:h-1/2 absolute top-0 left-0 z-[2] bg-gradient-to-r from-black to-transparent'></div>
