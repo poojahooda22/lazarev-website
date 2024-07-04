6ebcb11c066361a4d3c97ad27026d97dd7f63def
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-07-04 20:56:01 +0530

    added

--- components/Project.tsx
+++ components/Project.tsx
@@ -28,7 +28,8 @@ const Project = ({item}: any) => {
             </div>
         </div>
         <div 
-            className='absolute w-[8vw] h-[8vw]'>
+            className='absolute w-[8vw] h-[8vw]'
+            >
             <Image 
                 src={item.image} 
                 alt='cover' 
