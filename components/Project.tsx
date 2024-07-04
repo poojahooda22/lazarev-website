0a10c60809fd30af4ea320e2227ae2c737a69c25
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-07-04 21:09:43 +0530

    added

--- components/Project.tsx
+++ components/Project.tsx
@@ -18,7 +18,8 @@ const Project = ({item}: any) => {
             </h1>
         </div>
         <div className='flex items-center gap-[1.4vw]'>
-            <p className='text-grey text-[.8vw]'>{item.time} </p>
+            <p className='text-grey text-[.8vw]'>
+                {item.time} </p>
             <div className='w-[.9vw]'>
                 <svg fill="none" viewBox="0 0 15 15">
                     <path fill="currentColor" fill-rule="evenodd" d="M10.125 3.153H1.961V1H13.8v11.839h-2.152V4.675L2.722 13.6 1.2 12.078l8.925-8.925Z" clip-rule="evenodd"></path>
