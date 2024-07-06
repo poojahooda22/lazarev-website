e05b66518c52c041da74d5b9eafc0eba743d9d6e
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-07-06 21:55:22 +0530

    updated projects

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -32,7 +32,8 @@ const Projects = () => {
       <div className='w-full  max-w-screen-xl mx-auto border-t-[.1px] border-grey pt-[1.5vw] flex'>
         <div className='w-1/2 h-[34vw] flex flex-col items-start justify-between'>
           <div className='w-1/2 pt-[1vw]'>
-            <p className='w-1/2 text-[.8vw] leading-[1vw]'>Exclusive insights into<br/> UI/UX and product design</p>
+            <p className='w-1/2 text-[.8vw] leading-[1vw]'>
+            Exclusive insights into<br/> UI/UX and product design</p>
           </div>
           <div className='w-2/4'>
             <p className='text-sm leading-[1.3vw]'>
