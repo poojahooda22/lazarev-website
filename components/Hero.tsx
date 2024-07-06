60a92ef2100ac4aee5683af632b677aa03798ad1
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-07-06 22:28:21 +0530

    added

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -71,7 +71,8 @@ function Hero() {
                 <div  className='firsth1 overflow-hidden'>
                     <h1 
                         ref={headerRef} 
-                        className='sm:inline-flex items-center'>
+                        className='sm:inline-flex items-center'
+                        >
                         <span className='block sm:mr-8'>AI & ML </span>Pr
                         <span 
                             onMouseEnter={handleScale}
