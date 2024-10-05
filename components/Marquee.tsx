1bc82bb252b3c6e9ac7fa1a0368f6858f2acb3a0
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-10-05 11:16:51 +0530

    updated marquee

--- components/Marquee.tsx
+++ components/Marquee.tsx
@@ -8,7 +8,7 @@ function Marquee({imagesurl, direction}: {imagesurl: any, direction: any}) {
         <div className=' flex items-center justify-center gap-[10vw] sm:gap-[3vw] py-[1.5vw] sm:py-[1.5vw] overflow-hidden'>
             <motion.div 
               initial={{ x: direction === 'left' ? '0' : '-100%'}}
-                animate={{ x: direction === 'left' ? '-100%' : '0'}}
+              animate={{ x: direction === 'left' ? '-100%' : '0'}}
               transition={{ease: 'linear', duration: 30, repeat: Infinity}}
               className='flex flex-shrink-0 gap-[10vw] sm:gap-[3vw]'>
               {imagesurl.map((url: string, index: number) => <Image key={index} 
