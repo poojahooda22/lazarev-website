217aa7801e7f06ea3359433d1bf99282e4037785
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-10-05 11:15:38 +0530

    updated marquee

--- components/Marquee.tsx
+++ components/Marquee.tsx
@@ -17,7 +17,7 @@ function Marquee({imagesurl, direction}: {imagesurl: any, direction: any}) {
 
             <motion.div 
               initial={{ x: direction === 'left' ? '0' : '-100%'}}
-                animate={{ x: direction === 'left' ? '-100%' : '0'}}
+              animate={{ x: direction === 'left' ? '-100%' : '0'}}
                 transition={{ease: 'linear', duration: 30, repeat: Infinity}}
                 className='flex flex-shrink-0 gap-[10vw] sm:gap-[3vw]'>
               {imagesurl.map((url: string, index: number) =>
