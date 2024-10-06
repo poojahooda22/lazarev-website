c4026e25cfac93540efc41452cf30fa4aaa5ed27
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-10-06 11:31:52 +0530

    updated projects

--- components/Project.tsx
+++ components/Project.tsx
@@ -2,15 +2,45 @@
 
 import Image from 'next/image'
 import React, { useEffect, useRef } from 'react'
-import { gsap, Power4 } from 'gsap';
+import { gsap } from 'gsap';
 import { useGSAP } from '@gsap/react';
+import { Power4} from 'gsap/gsap-core';
 
 
 const Project = ({item}: any) => {
 
-    useGSAP(() => {
-        
-    })
+    useEffect(() => {
+        let rotate = 0;
+        let diffrot = 0;
+        const list = document.querySelectorAll<HTMLDivElement>('.right-elem');
+      
+        list.forEach((el) => {
+          el.addEventListener('mouseenter', (dets: MouseEvent) => {
+            const elemRect = el.getBoundingClientRect();
+            const diff = dets.clientY - elemRect.top;
+            diffrot = dets.clientX - rotate;
+            rotate = dets.clientX;
+      
+            const picture = el.querySelector<HTMLDivElement>(".picture");
+            if (picture) {
+              gsap.to(picture, {
+                opacity: 1,
+                ease: Power4,
+                top: diff,
+                left: dets.clientX,
+                rotate: gsap.utils.clamp(-20, 20, diffrot * 0.2),
+              });
+            }
+          });
+      
+          el.addEventListener('mouseleave', () => {
+            const picture = el.querySelector<HTMLDivElement>(".picture");
+            if (picture) {
+              gsap.to(picture, { opacity: 0, ease: Power4, duration: 0.5 });
+            }
+          }); 
+        });
+      }, []);
    
 
   return (
@@ -38,7 +68,7 @@ const Project = ({item}: any) => {
             </div>
         </div>
         <div 
-            className='picture absolute w-[8vw] 
+            className='absolute w-[8vw] 
             h-[8vw]'
         >
             <Image 
@@ -46,7 +76,7 @@ const Project = ({item}: any) => {
                 alt='cover' 
                 width={1000} 
                 height={100} 
-                className=' w-full h-full
+                className='picture w-full h-full
                 object-cover rounded-full'
             />
         </div>
